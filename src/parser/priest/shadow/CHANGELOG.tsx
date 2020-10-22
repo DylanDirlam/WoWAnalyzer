e20a006c3e2286950b5ef6c7016a7d39aa12f9ee
Dylan Dirlam <dadirlam@hotmail.com>
2020-10-21 21:23:13 -0400

    Add Fortress of the Mind and Unfurling Darkness talent modules

--- src/parser/priest/shadow/CHANGELOG.tsx
+++ src/parser/priest/shadow/CHANGELOG.tsx
@@ -6,6 +6,7 @@ import SpellLink from 'common/SpellLink';
 import { change, date } from 'common/changelog';
 
 export default [
+  change(date(2020, 10, 20), <>Added <SpellLink id={SPELLS.UNFURLING_DARKNESS_TALENT.id} /> and <SpellLink id={SPELLS.FORTRESS_OF_THE_MIND_TALENT.id} /> modules.</>, [Adoraci]),
   change(date(2020, 10, 18), <>Converted legacy listeners to new event filters</>, Zeboot),
   change(date(2020, 10, 17), <>Updated for Shadowlands Pre-Patch.</>, [Adoraci]),
   change(date(2020, 9, 21), <>Removed Azerite Traits and Added Event Listeners, Centralized Constants, and Integration Tests. </>, [Sharrq]),
--- src/parser/priest/shadow/CombatLogParser.tsx
+++ src/parser/priest/shadow/CombatLogParser.tsx
@@ -17,6 +17,8 @@ import ShadowWordPain from './modules/spells/ShadowWordPain';
 import Dispersion from './modules/spells/Dispersion';
 import VampiricEmbrace from './modules/spells/VampiricEmbrace';
 // talents
+import FortressOfTheMind from './modules/talents/FortressOfTheMind';
+import UnfurlingDarkness from './modules/talents/UnfurlingDarkness';
 import TwistOfFate from './modules/talents/TwistOfFate';
 import VoidTorrent from './modules/talents/VoidTorrent';
 import ShadowCrash from './modules/talents/ShadowCrash';
@@ -48,6 +50,8 @@ class CombatLogParser extends MainCombatLogParser {
     vampiricEmbrace: VampiricEmbrace,
 
     // talents:
+    fortressOfTheMind: FortressOfTheMind,
+    unfurlingDarkness: UnfurlingDarkness,
     twistOfFate: TwistOfFate,
     voidTorrent: VoidTorrent,
     shadowCrash: ShadowCrash,
