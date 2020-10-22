6fcbfbfc5edd74bc143b1a2149daf4d084d50ff1
Dylan Dirlam <dadirlam@hotmail.com>
2020-10-21 21:17:51 -0400

    Add insanity icon

--- src/interface/layout/Footer/index.js
+++ src/interface/layout/Footer/index.js
@@ -67,6 +67,7 @@ const Footer = props => (
                     <li>Plus by Galaxicon</li>
                     <li>duration by Bohdan Burmich</li>
                     <li>link by arjuazka</li>
+                    <li>Orb Circle by Martin</li>
                   </ul>
                 </>
               )}
